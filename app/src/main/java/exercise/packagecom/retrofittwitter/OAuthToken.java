59a271fe9453a5c4bf4d718d600687e48760c5f8
abhijeet-15 <abhijeet.hcs@gmail.com>
2017-06-18 02:00:31 +0530

    First commit

---
+++ app/src/main/java/exercise/packagecom/retrofittwitter/OAuthToken.java
@@ -0,0 +1,31 @@
+package exercise.packagecom.retrofittwitter;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by abhijeet on 18/6/17.
+ */
+
+public class OAuthToken {
+
+
+    @SerializedName("access_token")
+    private String accessToken;
+
+    @SerializedName("token_type")
+    private String tokenType;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public String getAuthorization() {
+        return getTokenType() + " " + getAccessToken();
+    }
+
+
+}
